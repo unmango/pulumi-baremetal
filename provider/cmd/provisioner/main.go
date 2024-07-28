c1ee1852ce2fe1aee4045862fbda07da2c3d39e5
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-27 19:10:16 -0500

    Its working so far

--- provider/cmd/provisioner/main.go
+++ provider/cmd/provisioner/main.go
@@ -9,23 +9,26 @@ import (
 	"github.com/unmango/pulumi-baremetal/provider/pkg/provisioner"
 )
 
-var log = logger()
-
 var (
 	address string
 	network string
+	verbose bool
+	log     *slog.Logger
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "provisioner",
 	Short: "The pulumi-baremetal provisioner",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		log = logger(verbose)
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		lis, err := net.Listen(network, address)
 		if err != nil {
 			return err
 		}
 
-		log.Info("Serving requests", "network", network, "address", address)
+		log.Info("serving", "network", network, "address", address, "verbose", verbose)
 		return provisioner.Serve(lis)
 	},
 }
@@ -33,16 +36,23 @@ var rootCmd = &cobra.Command{
 func main() {
 	rootCmd.Flags().StringVar(&address, "address", "", "Must be a valid `net.Listen()` address.")
 	rootCmd.Flags().StringVar(&network, "network", "tcp", "Must be a valid `net.Listen()` network. i.e. \"tcp\", \"tcp4\", \"tcp6\", \"unix\" or \"unixpacket\"")
+	rootCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Log verbosity")
 
-	log.Debug("Starting provisioner")
 	if err := rootCmd.Execute(); err != nil {
 		log.Error("provisioner failed", "err", err)
 	}
+
+	log.Debug("Exiting gracefully")
 }
 
-func logger() *slog.Logger {
+func logger(verbose bool) *slog.Logger {
+	var level slog.Level
+	if verbose {
+		level = slog.LevelDebug
+	}
+
 	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
+		Level: level,
 	})
 
 	return slog.New(handler)
