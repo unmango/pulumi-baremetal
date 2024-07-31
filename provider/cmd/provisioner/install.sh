1708d6eaafbe31bfbb0ed8621733e96d27cb1e9a
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-31 04:13:47 -0500

    Make the install script more better (#68)
    * Updated binary naming and archive structure
    
    The binary naming convention has been updated to include OS and architecture information. The archive name template has also been modified for better organization. Additionally, extra files have been added to the release section, including provisioner distributions, an install script, and a service file.
    
    * Added release artifact generation
    
    The workflow has been updated to include a step for generating release artifacts. This involves creating 'install.sh' and 'baremetal-provisioner.service'. The Makefile has also been modified to handle the creation of these new artifacts.
    
    * Added systemd service and improved install script
    
    Significant changes include:
    - Added a new systemd service file for the Pulumi Bare Metal Provisioner.
    - Enhanced the installation script with additional checks, logging, and functionality. This includes fetching the systemd unit file if it doesn't exist locally, creating a configuration directory and vars file, and setting up environment variables.
    - Improved user feedback messages in the installation script to provide more clarity.
    
    * Refactor provisioner install script
    
    The install.sh script in the provisioner command has been refactored for better readability and maintainability. The changes include:
    - Extracted variable printing into a separate function, printVars.
    - Simplified directory creation logic by removing unnecessary checks.
    - Added handling for DEV_MODE in fetching systemd unit file.
    - Included steps to reload systemd and enable service after configuration updates.
    
    * Fix the build
    
    And also some other stuff

--- provider/cmd/provisioner/install.sh
+++ provider/cmd/provisioner/install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -eu
 
+function require() {
+	if [ -z "$1" ]; then
+		echo "$2 is required"
+		exit 1
+	fi
+}
+
 # https://stackoverflow.com/questions/45125516/possible-values-for-uname-m
 function arch() {
 	case "$(uname -m)" in
@@ -43,6 +50,9 @@ if $IS_GIT; then
 		WORK="$GIT_ROOT/hack/.work"
 		mkdir -p "$WORK"
 		INSTALL_DIR="${INSTALL_DIR:-$WORK}"
+		CONFIG_DIR="${CONFIG_DIR:-$WORK}"
+		LISTEN_ADDRESS='localhost:6969'
+		SYSTEMD_SERVICE_FILE="${SYSTEMD_SERVICE_FILE:-$WORK/baremetal.service}"
 	else
 		echo 'Skipping when in a git repository'
 		exit 0
@@ -59,6 +69,7 @@ if [ -n "${CI:-}" ]; then
 	fi
 fi
 
+require "${LISTEN_ADDRESS:-}" 'LISTEN_ADDRESS'
 WORK="${WORK:-"$(mktemp --tmpdir -d pulumi-baremetal-XXXX)"}"
 LOGS="$WORK/logs.txt"
 
@@ -76,6 +87,10 @@ SRC_BIN="${SRC_BIN:-provisioner}"
 BIN_NAME="${BIN_NAME:-$SRC_BIN}"
 ARCHIVE_TMPL="pulumi-resource-baremetal-\$VERSION-$OS-$ARCH.tar.gz"
 INSTALL_DIR="${INSTALL_DIR:-/usr/local/bin}"
+SYSTEMD_DIR='/etc/systemd/system'
+SYSTEMD_SERVICE_FILE="${SYSTEMD_SERVICE_FILE:-$SYSTEMD_DIR/baremetal-provisioner.service}"
+CONFIG_DIR="${CONFIG_DIR:-$SYSTEMD_SERVICE_FILE.d}"
+VARS_FILE="$CONFIG_DIR/vars.env"
 
 function latestRelease() {
 	if [ ! -s "$LATEST_RELEASE_JSON" ]; then
@@ -85,39 +100,27 @@ function latestRelease() {
 	cat "$LATEST_RELEASE_JSON"
 }
 
-function main() {
-	if $DEV_MODE; then
-		echo '🚧 DEV_MODE=true, Carry on friend'
-	elif theyReadTheDocs; then
-		echo '🤗 Thanks for reading scripts instead of blindly executing bits from the internet!'
-	fi
-
-	if [ -z "$VERSION" ]; then
-		log 'VERSION unset, fetching latest release'
-		VERSION="$(latestRelease | $JQ -r '.tag_name')"
-	fi
-
+function printVars() {
 	echo '🔥 Running with vars:'
 	log "VERSION=$VERSION"
 	log "OS=$OS, ARCH=$ARCH"
 	log "INSTALL_DIR=$INSTALL_DIR"
+	log "LISTEN_ADDRESS=$LISTEN_ADDRESS"
 	log "WORK=$WORK"
 	log "BIN_NAME=$BIN_NAME"
+	log "SYSTEMD_SERVICE_FILE=$SYSTEMD_SERVICE_FILE"
 	log "SRC_BIN=$SRC_BIN"
 	log "GITHUB=$GITHUB"
 	log "GITHUB_API=$GITHUB_API"
+	log "VARS_FILE=$VARS_FILE"
+}
 
-	log 'Testing install directory'
-	if ! mkdir -p "$INSTALL_DIR"; then
-		log "Failed to mkdir -p $INSTALL_DIR"
-		exit 1
-	fi
-
+function installProvisioner() {
 	BIN="$INSTALL_DIR/$BIN_NAME"
 	if [ -f "$BIN" ]; then
 		log "$BIN"
 		echo "👻 Up to date"
-		exit 0
+		return 0
 	fi
 
 	ARCHIVE_NAME="$(VERSION="$VERSION" envsubst <<<"$ARCHIVE_TMPL")"
@@ -129,8 +132,58 @@ function main() {
 	curl -L "$URL" | tar -zx -C "$INSTALL_DIR" "$SRC_BIN" --transform "s/$SRC_BIN/$BIN_NAME/"
 
 	chmod +x "$BIN"
-	echo '⭐ Done'
+	echo 'Provisioner downloaded'
 	log "$BIN"
+}
+
+function main() {
+	if $DEV_MODE; then
+		echo '🚧 DEV_MODE=true, Carry on friend'
+	elif theyReadTheDocs; then
+		echo '🤗 Thanks for reading scripts before you execute!'
+	fi
+
+	if [ -z "$VERSION" ]; then
+		log 'VERSION unset, fetching latest release'
+		VERSION="$(latestRelease | $JQ -r '.tag_name')"
+	fi
+
+	printVars
+
+	log 'Ensuring install directory'
+	mkdir -p "$INSTALL_DIR"
+
+	installProvisioner
+
+	if [ ! -f "$SYSTEMD_SERVICE_FILE" ]; then
+		if $DEV_MODE && $IS_GIT; then
+			FILE="$GIT_ROOT/provider/cmd/provisioner/baremetal-provisioner.service"
+			sed "s@\$VARS_FILE@$VARS_FILE@g" "$FILE" >"$SYSTEMD_SERVICE_FILE"
+		else
+			URL="$GITHUB/releases/download/$VERSION/baremetal-provisioner.service"
+			log 'Fetching systemd unit file'
+			curl -fL "$URL" | sed "s@\$VARS_FILE@$VARS_FILE@g" >"$SYSTEMD_SERVICE_FILE"
+		fi
+	fi
+
+	log 'Ensuring config directory'
+	mkdir -p "$CONFIG_DIR"
+
+	log 'Creating vars file'
+	TMP_VARS="$(mktemp --tmpdir XXXX.env)"
+	echo "PROVISIONER_BIN=$BIN" | tee -a "$TMP_VARS"
+	echo "LISTEN_ADDRESS=$LISTEN_ADDRESS" | tee -a "$TMP_VARS"
+	mv "$TMP_VARS" "$VARS_FILE"
+
+	if $DEV_MODE; then exit 0; fi
+
+	log 'Reloading systemd'
+	systemctl daemon-reload
+
+	log 'Enabling service'
+	systemctl enable --now baremetal-provisioner
+
+	echo '⭐ Done'
 }
 
 function theyReadTheDocs() {
@@ -140,12 +193,14 @@ function theyReadTheDocs() {
 		echo ''
 		echo 'This script would like to create or modify these files:'
 		echo "$INSTALL_DIR/$BIN_NAME"
+		echo "$SYSTEMD_SERVICE_FILE"
+		echo "$VARS_FILE"
 		echo ''
 		echo 'And make requests to these urls:'
 		echo "$GITHUB"
 		echo "$GITHUB_API"
 		echo ''
-		echo 'If this is expected, set update your env and run this script again'
+		echo 'If this is expected, update your env and run this script again'
 		exit 1
 	fi
 }
