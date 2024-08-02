c3e84ef61a96c803ac75f3d9b49e38e97b028a67
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-08-02 17:57:21 -0500

    Added NoOp and If functions to opts
    Two new utility functions have been added to the opts package. The NoOp function is a no-operation function that returns an error-free function for any type. The If function takes a boolean predicate and an option, returning the option if the predicate is true or a no-op otherwise.

--- provider/pkg/internal/opts/opts.go
+++ provider/pkg/internal/opts/opts.go
@@ -13,3 +13,17 @@ func Apply[V O[T], T any](x *T, opts ...V) error {
 
 	return nil
 }
+
+func NoOp[V O[T], T any]() V {
+	return func(*T) error {
+		return nil
+	}
+}
+
+func If[V O[T], T any](predicate bool, o V) V {
+	if predicate {
+		return o
+	} else {
+		return NoOp[V]()
+	}
+}
