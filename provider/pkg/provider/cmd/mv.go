e82fd0955911f0622a2b085a908cf602a3b305de
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-08-04 00:37:24 -0500

    Added 'mv' command functionality
    A new file, mv.go, has been added to the cmd package. This file includes the implementation of a 'mv' command with various options such as backup, destination, directory, force and more. The MvArgs struct is defined to hold these options. Two methods Cmd() and ExpectedFiles() are implemented on this struct.
    
    The Mv type is also introduced with Create(), Update(), and Delete() methods implementing respective interfaces from infer package.
    
    Finally, the newly created 'mv' command is registered in provider.go by adding it to the list of inferred resources.

---
+++ provider/pkg/provider/cmd/mv.go
@@ -0,0 +1,112 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"path"
+
+	"github.com/pulumi/pulumi-go-provider/infer"
+	pb "github.com/unmango/pulumi-baremetal/gen/go/unmango/baremetal/v1alpha1"
+)
+
+type MvArgs struct {
+	Backup               string   `pulumi:"backup,optional"`
+	Destination          string   `pulumi:"destination,optional"`
+	Directory            string   `pulumi:"directory,optional"`
+	Force                bool     `pulumi:"force,optional"`
+	Help                 bool     `pulumi:"help,optional"`
+	NoClobber            bool     `pulumi:"noClobber,optional"`
+	NoTargetDirectory    bool     `pulumi:"noTargetDirectory,optional"`
+	Source               []string `pulumi:"source"`
+	StripTrailingSlashes bool     `pulumi:"stripTrailingSlashes,optional"`
+	Suffix               string   `pulumi:"suffix,optional"`
+	TargetDirectory      string   `pulumi:"targetDirectory,optional"`
+	Update               bool     `pulumi:"update,optional"`
+	Verbose              bool     `pulumi:"verbose,optional"`
+	Version              bool     `pulumi:"version,optional"`
+}
+
+// Cmd implements CommandArgs.
+func (m MvArgs) Cmd() *pb.Command {
+	b := builder{m.Source}
+	b.opv(m.Backup, "--backup")
+	b.op(m.Force, "--force")
+	b.op(m.NoClobber, "--no-clobber")
+	b.op(m.StripTrailingSlashes, "--strip-trailing-slashes")
+	b.opv(m.Suffix, "--suffix")
+	b.op(m.Update, "--update")
+	b.op(m.Verbose, "--verbose")
+	b.op(m.Version, "--version")
+
+	b.opv(m.TargetDirectory, "--target-directory")
+	b.op(m.NoTargetDirectory, "--no-target-directory")
+
+	b.arg(m.Destination)
+	b.arg(m.Directory)
+
+	return &pb.Command{
+		Bin:  pb.Bin_BIN_MV,
+		Args: b.args,
+	}
+}
+
+// ExpectedFiles implements CommandArgs.
+func (m MvArgs) ExpectedFiles() []string {
+	files := []string{}
+
+	mvSrc := func(d string) {
+		for _, f := range m.Source {
+			t := path.Join(d, path.Base(f))
+			files = append(files, t)
+		}
+	}
+
+	if m.Destination != "" {
+		files = append(files, m.Destination)
+	} else if m.Directory != "" {
+		mvSrc(m.Directory)
+	} else if m.TargetDirectory != "" {
+		mvSrc(m.TargetDirectory)
+	}
+
+	return files
+}
+
+var _ CommandArgs = MvArgs{}
+
+type Mv struct{}
+
+type MvState = CommandState[MvArgs]
+
+// Create implements infer.CustomCreate.
+func (Mv) Create(ctx context.Context, name string, inputs MvArgs, preview bool) (id string, output MvState, err error) {
+	state := MvState{}
+	if err := state.Create(ctx, inputs, preview); err != nil {
+		return name, state, fmt.Errorf("mv: %w", err)
+	}
+
+	return name, state, nil
+}
+
+// Update implements infer.CustomUpdate.
+func (Mv) Update(ctx context.Context, id string, olds MvState, news MvArgs, preview bool) (MvState, error) {
+	state, err := olds.Update(ctx, news, preview)
+	if err != nil {
+		return olds, fmt.Errorf("mv: %w", err)
+	}
+
+	return state, nil
+}
+
+// Delete implements infer.CustomDelete.
+func (Mv) Delete(ctx context.Context, id string, props MvState) error {
+	if err := props.Delete(ctx); err != nil {
+		return fmt.Errorf("mv: %w", err)
+	}
+
+	return nil
+}
+
+var _ = (infer.CustomCreate[MvArgs, MvState])((*Mv)(nil))
+var _ = (infer.CustomUpdate[MvArgs, MvState])((*Mv)(nil))
+var _ = (infer.CustomDelete[MvState])((*Mv)(nil))
