ec7498d6257d001f27c67c5f38e1da0982dbead6
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-08-04 00:36:26 -0500

    Added arg function to builder struct
    A new method 'arg' has been added to the builder struct. This method appends a given string value to the 'args' slice of the builder instance.

--- provider/pkg/provider/cmd/builder.go
+++ provider/pkg/provider/cmd/builder.go
@@ -4,6 +4,10 @@ type builder struct {
 	args []string
 }
 
+func (b *builder) arg(value string) {
+	b.args = append(b.args, value)
+}
+
 func (b *builder) op(input bool, name string) {
 	if input {
 		b.add(name)
