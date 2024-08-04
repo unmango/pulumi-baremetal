bda87e1dd8db4921553c11c5661f60301ad6a5c8
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-08-04 01:50:05 -0500

    Added test for file move operation
    A new test case has been added to the service tests. This test checks the functionality of moving a file from one location to another. It creates a temporary directory and a file within it, moves the file to a new location within the same directory, and then verifies that the move was successful by checking various conditions such as exit code, stdout content, created files list, moved files map and existence of old/new files. The temporary directory is cleaned up after each test run.

--- provider/pkg/provisioner/cmd/service_test.go
+++ provider/pkg/provisioner/cmd/service_test.go
@@ -27,6 +27,48 @@ var _ = Describe("Grpc Server", func() {
 		Expect(service).NotTo(BeNil())
 	})
 
+	Describe("Mv", func() {
+		var work string
+
+		BeforeEach(func() {
+			tmp, err := os.MkdirTemp("", "")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(tmp).To(BeADirectory())
+			work = tmp
+		})
+
+		It("should move a file", func(ctx context.Context) {
+			file := path.Join(work, "mv.txt")
+			newFile := path.Join(work, "mv-new.txt")
+			err := os.WriteFile(file, []byte("NA"), os.ModePerm)
+			Expect(err).NotTo(HaveOccurred())
+
+			res, err := service.Create(ctx, &pb.CreateRequest{
+				Command: &pb.Command{
+					Bin:  pb.Bin_BIN_MV,
+					Args: []string{file, newFile},
+				},
+				ExpectMoved: map[string]string{
+					file: newFile,
+				},
+			})
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(res).NotTo(BeNil())
+			Expect(res.Result.ExitCode).To(BeEquivalentTo(0))
+			Expect(res.Result.Stdout).To(BeEmpty())
+			Expect(res.CreatedFiles).To(BeEmpty())
+			Expect(res.MovedFiles).To(HaveKeyWithValue(file, newFile))
+			Expect(file).NotTo(BeARegularFile())
+			Expect(newFile).To(BeARegularFile())
+		})
+
+		AfterEach(func() {
+			err := os.RemoveAll(work)
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
 	Describe("Wget", func() {
 		var work string
 
