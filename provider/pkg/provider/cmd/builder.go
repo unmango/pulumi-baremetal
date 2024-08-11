ec914163ac3b79ae00a547d659cf391dd7042109
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-08-11 01:00:10 -0500

    Enhanced command builder functionality
    The command builder has been significantly improved. New fields have been added to the 'B' struct, including a binary and standard input field. The 'Cmd' function now returns a Command object with these new fields. Additionally, methods for setting the binary and standard input fields have been introduced. This enhancement provides more flexibility when constructing commands.

--- provider/pkg/provider/cmd/builder.go
+++ provider/pkg/provider/cmd/builder.go
@@ -1,13 +1,29 @@
 package cmd
 
+import pb "github.com/unmango/pulumi-baremetal/gen/go/unmango/baremetal/v1alpha1"
+
 type B struct {
-	Args []string
+	bin   *pb.Bin
+	stdin *string
+	Args  []string
+}
+
+func (b *B) Cmd() *pb.Command {
+	return &pb.Command{
+		Bin:   *b.bin,
+		Args:  b.Args,
+		Stdin: b.stdin,
+	}
 }
 
 func (b *B) Arg(value string) {
 	if value != "" {
 		b.Args = append(b.Args, value)
 	}
+}
+
+func (b *B) Bin(bin pb.Bin) {
+	b.bin = &bin
 }
 
 func (b *B) Op(input bool, name string) {
@@ -20,6 +36,10 @@ func (b *B) Opv(value, name string) {
 	if value != "" {
 		b.add(name, value)
 	}
+}
+
+func (b *B) Stdin(value string) {
+	b.stdin = &value
 }
 
 func (b *B) add(parts ...string) {
